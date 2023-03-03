b204ebb71c19b6554bba945b54c8830a66615da7
ZachMommaerts <zmommaerts@gmail.com>
2023-03-03 14:17:34 -0600

    Initial Commit

---
+++ Swift_Arrays/Swift_ArraysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ArraysApp.swift
+//  Swift_Arrays
+//
+//  Created by Zach Mommaerts on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ArraysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
